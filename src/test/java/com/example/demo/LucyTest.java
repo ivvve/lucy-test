0626bb11a67355c9c592572db9ee71a2a214a023
devson <sonyc5720@gmail.com>
2020-04-27 11:51:29 +0900

    test: added test template

--- src/test/java/com/example/demo/LucyTest.java
+++ src/test/java/com/example/demo/LucyTest.java
@@ -9,9 +9,16 @@ public class LucyTest {
 
 	@Test
 	public void xssEscapeTest() {
+		// given
 		String dirty = "<script>alert('dirty');</script>";
-		String clean = XssSaxFilter.getInstance().doFilter(dirty).replace("<!-- Not Allowed Tag Filtered -->", "");
 
+		// when
+		String clean = XssSaxFilter.getInstance().doFilter(dirty);
+		System.out.println(clean);
+		clean = clean.replace("<!-- Not Allowed Tag Filtered -->", "");
+		System.out.println(clean);
+
+		// then
 		assertThat(clean).isNotEqualTo(dirty);
 		assertThat(clean.contains("<")).isEqualTo(false);
 		assertThat(clean.contains(">")).isEqualTo(false);
